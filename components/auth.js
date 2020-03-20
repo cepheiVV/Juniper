b533ea297d7de6e34410dd7dec9c674b8d3a5790
Patrick Crausaz <crausaz.patrick@gmail.com>
2020-03-20 13:40:06 +0700

    Calculate and output the total pending income

--- components/auth.js
+++ components/auth.js
@@ -1,18 +1,17 @@
-const puppeteer = require('puppeteer');
-const config = require('../config.js');
+const puppeteer = require("puppeteer");
+const config = require("../config.js");
 
 const Auth = {
-
   /**
    * log in
    * @return {object} puppeteer browser and page
    */
   async logIn() {
-    const {username, password} = config.bonsai.auth;
+    const { username, password } = config.bonsai.auth;
 
-    const browser = await puppeteer.launch({headless: true});
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
-    await page.goto(config.bonsai.url.signIn, { waitUntil: 'networkidle2' });
+    await page.goto(config.bonsai.url.signIn, { waitUntil: "networkidle2" });
 
     await page.type('input[id="login-user-email"]', username);
     await page.type('input[id="login-user-password"]', password);
@@ -20,22 +19,21 @@ const Auth = {
       page.waitForNavigation(),
       page.click('input[name="commit"]')
     ]);
-    
-    return {browser, page};
+
+    return { browser, page };
   },
 
   /**
    * logOut
-   * closing the browser will terminate the 
+   * closing the browser will terminate the
    * session and bonsai will be loged out
-   * 
+   *
    * @param  {[type]} browser
    * @return {bool} true
    */
   async logOut(browser, page) {
     await browser.close();
   }
-
-}
+};
 
 module.exports = Auth;
--- components/cliReport.js
+++ components/cliReport.js
@@ -1,34 +1,42 @@
-const config = require('../config.js');
+const config = require("../config.js");
 
 const CliReport = {
-  
   async start() {
-    console.log('....................................');
-    console.log('.     GETTING DATA FROM BONSAI     .');
-    console.log('....................................');
+    console.log("....................................");
+    console.log(".     GETTING DATA FROM BONSAI     .");
+    console.log("....................................");
   },
-  
-  async report(dueInvoices, draftedInvoices, unbilledAndNoInvoiceAmount) {
-    const {currency} = config.settings;
 
-    console.log('.                                  .');
-    console.log('.             REPORT               .');
-    console.log('.                                  .');
-    console.log('. Invoice due          : ', dueInvoices, currency);
-    console.log('. Invoice drafted      : ', draftedInvoices, currency);
-    console.log('. Not on invoice yet   : ', unbilledAndNoInvoiceAmount, currency);
-    
-    console.log('.                                  .');
-    console.log('.                                  .');
+  async report(dueInvoices, draftedInvoices, unbilledAndNoInvoiceAmount) {
+    const { currency } = config.settings;
+
+    console.log(".                                  .");
+    console.log(".             REPORT               .");
+    console.log(".                                  .");
+    console.log(". Invoice due          : ", dueInvoices, currency);
+    console.log(". Invoice drafted      : ", draftedInvoices, currency);
+    console.log(
+      ". Not on invoice yet   : ",
+      unbilledAndNoInvoiceAmount,
+      currency
+    );
+    console.log(
+      ". Total income         : ",
+      draftedInvoices + unbilledAndNoInvoiceAmount,
+      currency
+    );
+
+    console.log(".                                  .");
+    console.log(".                                  .");
 
     return true;
   },
 
   async stop() {
-    console.log('....................................');
-    console.log('.      LOGGED OUT FROM BONSAI      .');
-    console.log('....................................');
+    console.log("....................................");
+    console.log(".      LOGGED OUT FROM BONSAI      .");
+    console.log("....................................");
   }
-}
+};
 
 module.exports = CliReport;
